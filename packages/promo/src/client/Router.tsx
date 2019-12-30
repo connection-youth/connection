939e8beb836f6f750282f5610acf430fce24793b
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-12-30 13:11:03 +0900

    feat: change Router route rendering

--- packages/promo/src/client/Router.tsx
+++ packages/promo/src/client/Router.tsx
@@ -3,45 +3,17 @@ import { Route } from 'react-router-dom';
 
 import { About, Contact, Home, Join, News } from '../pages';
 
-const routes = [
-  {
-    component: About.Org,
-    path: '/about/org',
-  },
-  {
-    // 가입 신청
-    component: Join.Form,
-    path: '/join/form',
-  },
-  {
-    // 공지사항
-    component: News.Notice,
-    path: '/news/notice',
-  },
-  {
-    // 보도자료
-    component: News.Report,
-    path: '/news/report',
-  },
-  {
-    // 외부공시
-    component: News.External,
-    path: '/news/external',
-  },
-  {
-    component: Contact,
-    path: '/contact',
-  },
-];
-
 class Router extends React.Component {
   public render() {
     return (
       <div>
         <Route exact={true} path="/" component={Home} />
-        {routes.map(({ path, component }, idx) =>
-          <Route path={path} component={component} key={idx} />,
-        )}
+        <Route path="/about/org" component={About.Org} />
+        <Route path="/join/form" component={Join.Form} />
+        <Route path="/news/notice" component={News.Notice} />
+        <Route path="/news/report" component={News.Report} />
+        <Route path="/news/external" component={News.External} />
+        <Route path="/contact" component={Contact} />
       </div>
     );
   }
