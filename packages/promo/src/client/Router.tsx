8b809ceabd2285515231be2f0e703296c82667f7
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-12-31 16:15:29 +0900

    feat: add routes for all pages

--- packages/promo/src/client/Router.tsx
+++ packages/promo/src/client/Router.tsx
@@ -8,11 +8,37 @@ class Router extends React.Component {
     return (
       <div>
         <Route exact={true} path="/" component={Home} />
+
+        {/* 커넥션 소개 */}
+        <Route exact path="/about" component={About.Info} />
+        {/* 대표 소개 */}
+        <Route path="/about/rep" component={About.Rep} />
+        {/* 조직도 및 직원 */}
         <Route path="/about/org" component={About.Org} />
+        {/* 자문위원 및 멘토 */}
+        <Route path="/about/mentor" component={About.Mentor} />
+        {/* 회원 */}
+        <Route path="/about/members" component={About.Members} />
+        {/* 파트너 목록 */}
+        <Route path="/about/partners" component={About.Partners} />
+
+        {/* 가입 안내 */}
+        <Route exact path="/join" component={Join.Main} />
+        {/* 가입 신청 */}
         <Route path="/join/form" component={Join.Form} />
+
+        {/* 공지사항 */}
         <Route path="/news/notice" component={News.Notice} />
+        {/* 소식 */}
+        <Route path="/news" component={News.Main} />
+        {/* 외부 행사 정보 */}
+        <Route path="/news" component={News.Event} />
+        {/* 보도자료 */}
         <Route path="/news/report" component={News.Report} />
+        {/* 외부공시 */}
         <Route path="/news/external" component={News.External} />
+
+        {/* 연락 */}
         <Route path="/contact" component={Contact} />
       </div>
     );
