f81ba41ab3cc2974ab355d88b937c5a99bad13ec
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2020-01-07 21:11:11 +0900

    feat: apply footer content

--- packages/promo/src/components/molecules/Footer.tsx
+++ packages/promo/src/components/molecules/Footer.tsx
@@ -2,7 +2,44 @@ import * as React from 'react';
 import styled from 'styled-components';
 
 const Footer = () => (
-  <Container />
+  <Container>
+    <Wrapper>
+      <Top>
+        <Text>
+          <strong>커넥션 네트워크</strong>
+        </Text>
+        <Text>
+          (가칭) 커넥션 재능기부단 사회적협동조합
+        </Text>
+      </Top>
+      <Contact>
+        <Text>
+          <strong>TEL</strong>
+          {' '}0507-1304-9899
+        </Text>
+        <Text>
+          <strong>FAX</strong>
+          {' '}0504-486-9899
+        </Text>
+        <Text>
+          <strong>Email</strong>
+          {' '}support@connectionyouth.org
+        </Text>
+      </Contact>
+      <Links>
+        <Text>
+          <strong>대표</strong>
+          {' '}이제우
+        </Text>
+        <Text>
+          <Link>개인정보처리방침</Link>
+        </Text>
+        <Text>
+          <Link>Contact Us</Link>
+        </Text>
+      </Links>
+    </Wrapper>
+  </Container>
 );
 
 export default Footer;
@@ -13,11 +50,73 @@ const Container = styled.footer`
   background-color: #414141;
   display: flex;
   justify-content: center;
-  align-items: center;
-  font-family: SegoeUI;
-  font-size: 1rem;
-  line-height: 1.33;
-  color: #ffffff;
   position: absolute;
   bottom: 0;
+  align-items: flex-start;
+  font-family: SegoeUI;
+  flex-direction: column;
+`;
+
+const Wrapper = styled.div`
+  width: 65%;
+  margin: 0 auto;
+
+  @media (max-width: 840px) {
+    width: 95%;
+  }
+`;
+
+const Top = styled.div`
+  display: flex;
+`;
+
+const Text = styled.span`
+  position: relative;
+  font-family: SegoeUI;
+  font-size: .9rem;
+  font-weight: 300;
+  line-height: 1.33;
+  color: #ffffff;
+
+  @media (max-width: 650px) {
+    font-size: .8rem;
+
+    &:not(:last-child) {
+      margin-right: .5rem;
+      padding-right: .5rem;
+    }
+  }
+
+  &:not(:last-child) {
+    margin-right: .5rem;
+    padding-right: .5rem;
+  }
+
+  &:not(:last-child)::after {
+    content: "";
+    position: absolute;
+    top: 50%;
+    right: -2px;
+    transform: translateY(-50%);
+    width: 1px;
+    height: 1rem;
+    background: white;
+  }
+
+  strong {
+    font-weight: 700;
+  }
+`;
+
+const Contact = styled.div`
+  display: flex;
+`;
+
+const Links = styled.div`
+  display: flex;
+`;
+
+const Link = styled.a`
+  font-weight: 600;
+  color: #298fe3;
 `;
