a41a3ecf6284d6e4e79196a116ed6cf9c6c12536
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2020-01-07 18:35:27 +0900

    chore: add deploy script

---
+++ .github/workflows/deploy.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const sftp = require('node-sftp-deploy');
+
+async function deploy () {
+  await sftp({
+    user: process.env.SFTP_USERNAME,
+    pass: process.env.SFTP_PASSWORD,
+    host: process.env.SFTP_HOST,
+    port: 22,
+    remotePath: process.env.SFTP_CWD,
+    sourcePath: path.resolve(__dirname, '../dist'),
+  });
+};
+
+deploy()
+  .then(() => console.log(`🎉 Deploy Finished: ${process.env.SFTP_HOST}`))
+  .catch(err => console.error(err));
