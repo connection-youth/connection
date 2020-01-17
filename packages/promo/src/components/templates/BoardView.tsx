ff4f98c08c624155a601903f4982b2ff053c22d2
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2020-01-18 00:41:53 +0900

    feat(news): structure for board view

--- packages/promo/src/components/templates/BoardView.tsx
+++ packages/promo/src/components/templates/BoardView.tsx
@@ -1,9 +1,96 @@
 import * as React from 'react';
 import styled from 'styled-components';
 
-export const BoardView: React.FC = () => {
+export interface IContent {
+  id: number;
+  date: string;
+  title: string;
+  author: string;
+  views: number;
+  category: string;
+  file?: string;
+  article: string;
+}
+
+type BoardViewProps = {
+  content: IContent;
+};
+
+export const BoardView: React.FC<BoardViewProps> = ({ content }) => {
+  const { id, date, title, author, views, category, file = '', article } = content;
   return (
-    <Container />
+    <Container>
+      <ViewHead>
+        <SideInfo>{id}/130</SideInfo>
+        <SideInfo>{date}</SideInfo>
+      </ViewHead>
+
+      <Table.Container>
+        <Table.Head>
+          <Table.Row>
+            <Table.Heading>
+              제목
+            </Table.Heading>
+            <Table.Data>
+              {title}
+            </Table.Data>
+            <Table.Heading>
+              등록자
+            </Table.Heading>
+            <Table.Data>
+              {author}
+            </Table.Data>
+          </Table.Row>
+          <Table.Row>
+            <Table.Heading>
+              카테고리
+            </Table.Heading>
+            <Table.Data>
+              {category}
+            </Table.Data>
+            <Table.Heading>
+              조회수
+            </Table.Heading>
+            <Table.Data>
+              {views}
+            </Table.Data>
+          </Table.Row>
+          <Table.Row>
+            <Table.Heading>
+              첨부파일
+            </Table.Heading>
+            <Table.Data>
+              {file}
+            </Table.Data>
+          </Table.Row>
+        </Table.Head>
+      </Table.Container>
+
+      <Article>
+        {article}
+      </Article>
+
+      <Table.Container>
+        <Table.Head>
+          <Table.Row>
+            <Table.Heading>
+              다음글
+            </Table.Heading>
+            <Table.Data>
+              스타트업둥지 1st 성과공유회
+            </Table.Data>
+          </Table.Row>
+          <Table.Row>
+            <Table.Heading>
+              이전글
+            </Table.Heading>
+            <Table.Data>
+              [외부] KOICA와 르호봇이 함께 하는 한국-팔레스타인 네트워킹 데이
+            </Table.Data>
+          </Table.Row>
+        </Table.Head>
+      </Table.Container>
+    </Container>
   );
 };
 
@@ -11,3 +98,28 @@ export default BoardView;
 
 const Container = styled.div`
 `;
+
+const ViewHead = styled.div`
+`;
+
+const SideInfo = styled.span`
+`;
+
+const Article = styled.div`
+`;
+
+const Table = {
+  Container: styled.table`
+    width: 100%;
+    font-size: .78rem;
+    color: #575757;
+  `,
+  Head: styled.thead`
+  `,
+  Row: styled.tr`
+  `,
+  Heading: styled.th`
+  `,
+  Data: styled.td`
+  `,
+};
--- packages/promo/src/pages/News/NoticeView.tsx
+++ packages/promo/src/pages/News/NoticeView.tsx
@@ -1,8 +1,18 @@
 import * as React from 'react';
-import styled from 'styled-components';
 
 import Layout from '../../components/Layout';
-import BoardView from '../../components/templates/BoardView';
+import BoardView, { IContent } from '../../components/templates/BoardView';
+
+const content: IContent = {
+  id: 130,
+  date: '2019-07-27',
+  title: '커넥션 공지사항 커넥션 공지사항 커넥션 공지사항 커넥션 공지사항',
+  author: '관리자',
+  views: 1,
+  category: '회계 정보',
+  file: '2019년도 상반기 Connection 재무제표.pdf',
+  article: '응모 주제 및 지원 자격',
+};
 
 export const NoticeView: React.FC = () => {
   return (
@@ -10,7 +20,9 @@ export const NoticeView: React.FC = () => {
       title="공지사항"
       category="News"
     >
-      <BoardView />
+      <BoardView
+        content={content}
+      />
     </Layout>
   );
 };
